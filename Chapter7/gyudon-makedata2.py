9762961c730dfa6209b8a845a6be7f552ae52b87
sepakTama <ma.tama121096@gmail.com>
2020-08-27 21:29:00 +0900

    gyudon

--- Chapter7/gyudon-makedata2.py
+++ Chapter7/gyudon-makedata2.py
@@ -1,19 +1,21 @@
-# -*- coding: utf-8 -*-
 from PIL import Image
 import os, glob
 import numpy as np
 import random, math
 
-# 分類対象のカテゴリー
+# 分類対象のカテゴリを選ぶ 
 root_dir = "./image/"
 categories = ["normal", "beni", "negi", "cheese"]
 nb_classes = len(categories)
 image_size = 50
 
+# 画像データを読み込む --- (※1)
+X = [] # 画像データ
+Y = [] # ラベルデータ
 def add_sample(cat, fname, is_train):
     img = Image.open(fname)
-    img = img.convert("RGB")
-    img = img.resize((image_size, image_size))
+    img = img.convert("RGB") # カラーモードの変更
+    img = img.resize((image_size, image_size)) # 画像サイズの変更
     data = np.asarray(img)
     X.append(data)
     Y.append(cat)
@@ -25,13 +27,13 @@ def add_sample(cat, fname, is_train):
         data = np.asarray(img2)
         X.append(data)
         Y.append(cat)
-        # img2.save("gyudon-"+str(ang)+".PNG")
+        # img2.save("gyudon-"+str(ang)+".png")
         # 反転する
         img2 = img2.transpose(Image.FLIP_LEFT_RIGHT)
         data = np.asarray(img2)
         X.append(data)
         Y.append(cat)
-        
+
 def make_sample(files, is_train):
     global X, Y
     X = []; Y = []
@@ -39,21 +41,25 @@ def make_sample(files, is_train):
         add_sample(cat, fname, is_train)
     return np.array(X), np.array(Y)
 
-# ディレクトリごとに分けられたファイルを収集する
+# ディレクトリごとに分けられたファイルを収集する --- (※2)
 allfiles = []
 for idx, cat in enumerate(categories):
     image_dir = root_dir + "/" + cat
-    files = glob.glob(image_dir + "/*.PNG")
+    files = glob.glob(image_dir + "/*.jpg")
     for f in files:
         allfiles.append((idx, f))
-        
-# シャッフルして学習データとテストデータに分ける
+
+# シャッフルして学習データとテストデータに分ける --- (※3)
 random.shuffle(allfiles)
 th = math.floor(len(allfiles) * 0.6)
 train = allfiles[0:th]
-test = allfiles[th:]
+test  = allfiles[th:]
 X_train, y_train = make_sample(train, True)
 X_test, y_test = make_sample(test, False)
 xy = (X_train, X_test, y_train, y_test)
 np.save("./image/gyudon2.npy", xy)
-print("ok", len(y_train))
+print("ok,", len(y_train))
+
+
+
+
